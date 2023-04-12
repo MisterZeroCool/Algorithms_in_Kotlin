1f97cb94defdf4f4c85ae3dcecfaa9032ec7a606
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-04-12 04:26:52 +0300

    Sorting

---
+++ src/main/kotlin/sorting/BubbleSort.kt
@@ -0,0 +1,27 @@
+package sorting
+
+fun <T : Comparable<T>> Array<T>.bubbleSort() {
+    val array = this
+    for (i in 0 until size - 1) {
+        for (j in 0 until size - 1 - i) {
+            if (array[j] > array[j + 1]) {
+                array[j] = array[j + 1].apply {
+                    array[j + 1] = array[j]
+                }
+            }
+        }
+    }
+}
+
+fun <T : Comparable<T>> MutableList<T>.bubbleSort() {
+    val list = this
+    for (i in 0 until size - 1) {
+        for (j in 0 until size - 1 - i) {
+            if (list[j] > list[j + 1]) {
+                list[j] = list[j + 1].apply {
+                    list[j + 1] = list[j]
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/sorting/BubbleSortImproved.kt
@@ -0,0 +1,33 @@
+package sorting
+
+fun <T : Comparable<T>> Array<T>.bubbleSortImproved() {
+    val array = this
+    var isSorted = true
+    for (i in 0 until size - 1) {
+        for (j in 0 until size - 1 - i) {
+            if (array[j] > array[j + 1]) {
+                isSorted = false
+                array[j] = array[j + 1].apply {
+                    array[j + 1] = array[j]
+                }
+            }
+        }
+        if (isSorted) break
+    }
+}
+
+fun <T : Comparable<T>> MutableList<T>.bubbleSortImproved() {
+    val list = this
+    var isSorted = true
+    for (i in 0 until size - 1) {
+        for (j in 0 until size - 1 - i) {
+            if (list[j] > list[j + 1]) {
+                isSorted = false
+                list[j] = list[j + 1].apply {
+                    list[j + 1] = list[j]
+                }
+            }
+        }
+        if (isSorted) break
+    }
+}
